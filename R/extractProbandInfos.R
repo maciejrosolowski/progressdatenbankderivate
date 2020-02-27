5f323e737959c0c2ca1b0c9472f7c4e15405dea4
Maciej Rosolowski <maciej.rosolowski@imise.uni-leipzig.de>
2020-02-27 15:55:19 +0100

    The function extractProbandInfos added.

---
+++ R/extractProbandInfos.R
@@ -0,0 +1,34 @@
+#' get patient characteristics from the table DID_PROBAND
+#'
+#' @param DID_PROBAND data.table containing the table DID_PROBAND from the
+#' database of the PROGRESS study
+#' @param parameter a character vector with the names of the charcteristics
+#'  to be extracted.
+#'
+#' @return a data.table with the PATSTUID and the extracted or computed patient
+#' characteristics.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' excel_fn <- paste0("/net/ifs1/san_projekte/projekte/",
+#' "PROGRESS/Datenmanagement/Data_freezes/",
+#' "20190320/PROGRESS-freeze_201903_01.xlsx")
+#' DID_PROBAND <- readxl::read_excel(excel_fn, "DID_PROBAND")
+#' data.table::setDT(DID_PROBAND)
+#' extractProbandInfos(DID_PROBAND, parameter = "BMI")
+#' }
+extractProbandInfos = function(DID_PROBAND,
+                               parameter =  c('ZENTRUM', 'AGE', 'SEX',
+                                              'WEIGHT', 'HEIGHT', "BMI", 'COB',
+                                              'COBM', 'COBF',  'ABKDAYS_BEVOR',
+                                              'CAPSYS')) {
+  # due to non-standard evaluation notes in R CMD check
+  BMI <- HEIGHT <- SEX <- WEIGHT <- NULL
+  # parameter =  c('ZENTRUM', 'AGE', 'SEX', 'WEIGHT', 'HEIGHT', "BMI", 'COB',
+  # 'COBM', 'COBF',  'ABKDAYS_BEVOR', 'CAPSYS')
+  DID_PROBAND[,SEX := removeMissingCodes(SEX)]
+  DID_PROBAND[,BMI:= WEIGHT/(HEIGHT/100)^2]
+  message("'COB', 'COBM', 'COBF' means country of birth proband/mother/father")
+  DID_PROBAND[,c('PATSTUID', parameter), with = F]
+}
