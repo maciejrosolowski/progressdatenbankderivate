418dfa43ff47b69339eaec5fef64cd1c121ec65c
Maciej Rosolowski <maciej.rosolowski@imise.uni-leipzig.de>
2020-03-23 19:18:52 +0100

    small changes in the documentation

--- R/getData4verwirrt.R
+++ R/getData4verwirrt.R
@@ -14,19 +14,23 @@
 #' "20190320/PROGRESS-freeze_201903_01.xlsx")
 #' FRM_BEF <- readxl::read_excel(excel_fn, 'FRM_BEF')
 #' data.table::setDT(FRM_BEF)
-#' getData4verwirrt(FRM_BEF)
+#' toadd_verwirrt <- getData4verwirrt(FRM_BEF)
+#' toadd_verwirrt[]
 #' }
 getData4verwirrt <- function(FRM_BEF) {
   # due to non-standard evaluation notes in R CMD check
   PATSTUID <- EVENT<- VERWIRRTH <- value <- patstuid <- NULL
   # FRM_BEF[allDuplicatedEntries(paste(PATSTUID, EVENT))]
-  toadd_verwirrt = FRM_BEF[,.(patstuid = PATSTUID, event = EVENT,value= as.numeric(VERWIRRTH))]
+  toadd_verwirrt = FRM_BEF[,.(patstuid = PATSTUID, event = EVENT,
+                              value= as.numeric(VERWIRRTH))]
   toadd_verwirrt[value %in% c(-1,99) , value := NA]
   found_events_verwirrt = unique(toadd_verwirrt$event  )
   toadd_verwirrt = dcast.data.table(toadd_verwirrt, patstuid ~ event)
-  setnames(toadd_verwirrt,  as.character(found_events_verwirrt), paste0("verwirrt_",event2zeitpunkt(found_events_verwirrt, returnformat = "zp_fabianref")))
+  setnames(toadd_verwirrt,  as.character(found_events_verwirrt),
+           paste0("verwirrt_",event2zeitpunkt(found_events_verwirrt,
+                                              returnformat = "zp_fabianref")))
   # Hmisc::describe(toadd_verwirrt)
   # stopifnot(nrow(toadd_verwirrt[allDuplicatedEntries(patstuid)])==0)
   stopifnot(anyDuplicated(toadd_verwirrt[, patstuid]) == 0)
-  toadd_verwirrt
+  toadd_verwirrt[]
 }
