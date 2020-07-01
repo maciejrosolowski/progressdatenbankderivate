d146029b41eb47769d451cf70dca253662ffc1a1
Maciej Rosolowski <maciej.rosolowski@imise.uni-leipzig.de>
2020-07-01 23:41:11 +0200

    Implemented the 50% rule for curb65.fct.

--- R/curb65.fct.R
+++ R/curb65.fct.R
@@ -27,6 +27,8 @@
 #' scores. Columns PATSTUID and EVENT identify the patient and time point.
 #' Other columns contain data used for computing the components of the CURB65
 #' scores.
+#' If more than 50% of the subscores of each score are avaiable then the
+#  corresponding score is available. Otherwise it is NA.
 #' Note: If FRM_BEAT != NULL the calculation of the respiratory component of
 #' the CURB65 score differs from the original definition of CURB65. Patients
 #' who are ventilated are counted as if they their respiratory rate >= 30.
@@ -237,36 +239,37 @@ curb65_default<-function(verwirrt,bun,afrq.max,sysbp.min,diasbp.min,age){
                bp_curb65 = as.numeric(sysbp.min < 90 | diasbp.min <= 60),
                age_curb65 = as.numeric(age >= 65))
 
-  # sum(c(NA, NA), na.rm = TRUE) results in 0. "ifelse" to return NA instead.
+  # If more than 50% of the subscores of each score are avaiable then the
+  # corresponding score is available. Otherwise it is NA.
   curb_components[, crb :=
-                    ifelse(rowSums(is.na(.SD)) == 3,
+                    ifelse(rowSums(is.na(.SD)) >= 2,
                            NA_integer_, rowSums(.SD, na.rm = TRUE)),
                   .SDcols = c("verwirrt_curb65",
                               "afrq.max_curb65", "bp_curb65")]
   curb_components[, crb65 :=
-                    ifelse(rowSums(is.na(.SD)) == 4,
+                    ifelse(rowSums(is.na(.SD)) >= 2,
                            NA_integer_, rowSums(.SD, na.rm = TRUE)),
                   .SDcols = c("verwirrt_curb65",
                               "afrq.max_curb65", "bp_curb65",
                               "age_curb65")]
   curb_components[, curb :=
-                    ifelse(rowSums(is.na(.SD)) == 4,
+                    ifelse(rowSums(is.na(.SD)) >= 2,
                            NA_integer_, rowSums(.SD, na.rm = TRUE)),
                   .SDcols = c("verwirrt_curb65", "bun_curb65",
                               "afrq.max_curb65", "bp_curb65")]
   curb_components[, curb65 :=
-                    ifelse(rowSums(is.na(.SD)) == 5,
+                    ifelse(rowSums(is.na(.SD)) >= 3,
                            NA_integer_, rowSums(.SD, na.rm = TRUE)),
                   .SDcols = c("verwirrt_curb65", "bun_curb65",
                               "afrq.max_curb65", "bp_curb65",
                               "age_curb65")]
 
-  # if all components of the scores are NA then set the scores to 0
-  # uncomment it, if they should be NA instead
-  curb_components[is.na(crb), crb := 0]
-  curb_components[is.na(crb65), crb65 := 0]
-  curb_components[is.na(curb), curb := 0]
-  curb_components[is.na(curb65), curb65 := 0]
+  # # if all components of the scores are NA then set the scores to 0
+  # # uncomment it, if they should be NA instead
+  # curb_components[is.na(crb), crb := 0]
+  # curb_components[is.na(crb65), crb65 := 0]
+  # curb_components[is.na(curb), curb := 0]
+  # curb_components[is.na(curb65), curb65 := 0]
 
   # return(cbind(curb.vollst,curb, curb_components, is_na_df))
   # return(cbind(curb.vollst,curb, curb_components))
@@ -448,36 +451,37 @@ curb65_bea<-function(verwirrt,bun,afrq.max,sysbp.min,diasbp.min,age, patbea){
                bp_curb65 = as.numeric(sysbp.min < 90 | diasbp.min <= 60),
                age_curb65 = as.numeric(age >= 65))
 
-  # sum(c(NA, NA), na.rm = TRUE) results in 0. "ifelse" to return NA instead.
+  # If more than 50% of the subscores of each score are avaiable then the
+  # corresponding score is available. Otherwise it is NA.
   curb_components[, crb :=
-                    ifelse(rowSums(is.na(.SD)) == 3,
+                    ifelse(rowSums(is.na(.SD)) >= 2,
                            NA_integer_, rowSums(.SD, na.rm = TRUE)),
                   .SDcols = c("verwirrt_curb65",
                               "afrq.max_curb65", "bp_curb65")]
   curb_components[, crb65 :=
-                    ifelse(rowSums(is.na(.SD)) == 4,
+                    ifelse(rowSums(is.na(.SD)) >= 2,
                            NA_integer_, rowSums(.SD, na.rm = TRUE)),
                   .SDcols = c("verwirrt_curb65",
                               "afrq.max_curb65", "bp_curb65",
                               "age_curb65")]
   curb_components[, curb :=
-                    ifelse(rowSums(is.na(.SD)) == 4,
+                    ifelse(rowSums(is.na(.SD)) >= 2,
                            NA_integer_, rowSums(.SD, na.rm = TRUE)),
                   .SDcols = c("verwirrt_curb65", "bun_curb65",
                               "afrq.max_curb65", "bp_curb65")]
   curb_components[, curb65 :=
-                    ifelse(rowSums(is.na(.SD)) == 5,
+                    ifelse(rowSums(is.na(.SD)) >= 3,
                            NA_integer_, rowSums(.SD, na.rm = TRUE)),
                   .SDcols = c("verwirrt_curb65", "bun_curb65",
                               "afrq.max_curb65", "bp_curb65",
                               "age_curb65")]
 
-  # if all components of the scores are NA then set the scores to 0
-  # uncomment it, if they should be NA instead
-  curb_components[is.na(crb), crb := 0]
-  curb_components[is.na(crb65), crb65 := 0]
-  curb_components[is.na(curb), curb := 0]
-  curb_components[is.na(curb65), curb65 := 0]
+  # # if all components of the scores are NA then set the scores to 0
+  # # uncomment it, if they should be NA instead
+  # curb_components[is.na(crb), crb := 0]
+  # curb_components[is.na(crb65), crb65 := 0]
+  # curb_components[is.na(curb), curb := 0]
+  # curb_components[is.na(curb65), curb65 := 0]
 
   # return(cbind(curb.vollst,curb, curb_components, is_na_df))
   # return(cbind(curb.vollst,curb, curb_components))
